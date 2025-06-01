10c8ba94dc9554b3ed48fc2bacbe43c172c645ff
o9 <156221015+o9-9@users.noreply.github.com>
2025-06-01 21:03:19 +0300

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,70 @@
+# Ensure the script can run with elevated privileges
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "Please run this script as an Administrator!"
+    break
+}
+
+# Function to test internet connectivity
+function Test-InternetConnection {
+    try {
+        $testConnection = Test-Connection -ComputerName www.google.com -Count 1 -ErrorAction Stop
+        return $true
+    }
+    catch {
+        Write-Warning "Internet connection is required but not available. Please check your connection."
+        return $false
+    }
+}
+
+# Function to install CodingFonts
+function Install-CodingFonts {
+    param (
+        [string]$FontName = "CodingFonts",
+        [string]$FontDisplayName = "Coding Fonts"
+    )
+
+    try {
+        [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+        $fontFamilies = (New-Object System.Drawing.Text.InstalledFontCollection).Families.Name
+        if ($fontFamilies -notcontains "${FontDisplayName}") {
+            $fontZipUrl = "https://github.com/o9-9/files/raw/main/${FontName}.zip"
+            $zipFilePath = "$env:TEMP\${FontName}.zip"
+            $extractPath = "$env:TEMP\${FontName}"
+
+            $webClient = New-Object System.Net.WebClient
+            $webClient.DownloadFileAsync((New-Object System.Uri($fontZipUrl)), $zipFilePath)
+
+            while ($webClient.IsBusy) {
+                Start-Sleep -Seconds 2
+            }
+
+            Expand-Archive -Path $zipFilePath -DestinationPath $extractPath -Force
+            $destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
+            Get-ChildItem -Path $extractPath -Recurse -Filter "*.ttf" | ForEach-Object {
+                If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {
+                    $destination.CopyHere($_.FullName, 0x10)
+                }
+            }
+
+            Remove-Item -Path $extractPath -Recurse -Force
+            Remove-Item -Path $zipFilePath -Force
+        } else {
+            Write-Host "Font ${FontDisplayName} already installed"
+        }
+    }
+    catch {
+        Write-Error "Failed to download or install ${FontDisplayName} font. Error: $_"
+    }
+}
+
+# Check for internet connectivity before proceeding
+if (-not (Test-InternetConnection)) {
+    break
+}
+
+# Font Install
+Install-NerdFonts -FontName "CodingFonts" -FontDisplayName "Coding Fonts"
+
 # Installs the latest VS Code
 winget install --id Microsoft.VisualStudioCode --scope machine --accept-package-agreements --accept-source-agreements
 
@@ -29,7 +96,7 @@ Write-Host -ForegroundColor Green "✔ Environment variables refreshed."
 $extensionsJson = "$env:TEMP\extensions.json"
 Invoke-WebRequest -Uri "$repoUrl/extensions.json" -OutFile $extensionsJson
 $extensions = (Get-Content $extensionsJson | ConvertFrom-Json).extensions
-$extensions | ForEach-Object { 
+$extensions | ForEach-Object {
     code --install-extension $_
     Write-Host -ForegroundColor Cyan "✔ Installed $_"
 }
